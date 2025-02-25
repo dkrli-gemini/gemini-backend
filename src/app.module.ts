b5c3e8d0ec33ee440626adeb61d41c24194843aa
Pedro Martineli <martinelipedro@gmail.com>
2025-02-25 16:31:16 -0300

    feat: add signup controller

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,11 +1,12 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { AuthController } from './infra/auth/auth.controller';
 import { AuthModule } from './infra/auth/auth.module';
 
 @Module({
   imports: [AuthModule],
-  controllers: [AppController],
+  controllers: [AppController, AuthController],
   providers: [AppService],
 })
 export class AppModule {}
--- src/infra/auth/auth.module.ts
+++ src/infra/auth/auth.module.ts
@@ -1,5 +1,6 @@
 import { Module } from '@nestjs/common';
 import { PassportModule } from '@nestjs/passport';
+import { AuthController } from './auth.controller';
 import { KeycloakStrategy } from './keycloak.strategy';
 
 @Module({
