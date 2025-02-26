3d4cc5740607f34c9649805fb6a424bd219a7672
Pedro Martineli <martinelipedro@gmail.com>
2025-02-26 16:12:51 -0300

    feat: implement auth2.0 keycloak strategy

--- src/infra/auth/keycloak.strategy.ts
+++ src/infra/auth/keycloak.strategy.ts
@@ -1,31 +1,49 @@
 import { Injectable } from '@nestjs/common';
 import { PassportStrategy } from '@nestjs/passport';
-import { Strategy } from 'passport-keycloak-oauth2';
+import { Strategy } from 'passport-oauth2';
 import { ConfigService } from '@nestjs/config';
+import axios from 'axios';
 
 @Injectable()
-export class KeycloakOAuthStrategy extends PassportStrategy(
-  Strategy,
-  'keycloak',
-) {
-  constructor(configService: ConfigService) {
+export class KeycloakStrategy extends PassportStrategy(Strategy, 'keycloak') {
+  constructor(private readonly configService: ConfigService) {
     super({
+      authorizationURL: `${configService.get<string>('KEYCLOAK_URL')}/realms/${configService.get<string>('KEYCLOAK_REALM')}/protocol/openid-connect/auth`,
+      tokenURL: `${configService.get<string>('KEYCLOAK_URL')}/realms/${configService.get<string>('KEYCLOAK_REALM')}/protocol/openid-connect/token`,
       clientID: configService.get<string>('KEYCLOAK_CLIENT_ID'),
       clientSecret: configService.get<string>('KEYCLOAK_CLIENT_SECRET'),
       callbackURL: configService.get<string>('KEYCLOAK_CALLBACK_URL'),
-      authorizationURL: `${configService.get<string>('KEYCLOAK_URL')}/realms/${configService.get<string>('KEYCLOAK_REALM')}/protocol/openid-connect/auth`,
-      tokenURL: `${configService.get<string>('KEYCLOAK_URL')}/realms/${configService.get<string>('KEYCLOAK_REALM')}/protocol/openid-connect/token`,
-      userInfoURL: `${configService.get<string>('KEYCLOAK_URL')}/realms/${configService.get<string>('KEYCLOAK_REALM')}/protocol/openid-connect/userinfo`,
       scope: ['openid', 'profile', 'email'],
-      pkce: true, // Enable PKCE for better security
     });
   }
 
-  async validate(accessToken: string, refreshToken: string, profile: any) {
-    return {
-      accessToken,
-      refreshToken,
-      profile,
-    };
+  async validate(
+    accessToken: string,
+    refreshToken: string,
+    profile: any,
+    done: Function,
+  ) {
+    try {
+      const { data } = await axios.get(
+        `${this.configService.get<string>('KEYCLOAK_URL')}/realms/${this.configService.get<string>('KEYCLOAK_REALM')}/protocol/openid-connect/userinfo`,
+        {
+          headers: { Authorization: `Bearer ${accessToken}` },
+        },
+      );
+
+      const user = {
+        accessToken,
+        refreshToken,
+        id: data.sub,
+        username: data.preferred_username,
+        email: data.email,
+        firstName: data.given_name,
+        lastName: data.family_name,
+      };
+
+      done(null, user);
+    } catch (error) {
+      done(error, false);
+    }
   }
 }
