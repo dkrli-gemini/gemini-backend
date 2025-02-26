c1ced271667094fa3acef162fb47d37312597b79
Pedro Martineli <martinelipedro@gmail.com>
2025-02-26 15:46:16 -0300

    feat: add IUseCase contract

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,12 +1,11 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
-import { AuthController } from './infra/auth/auth.controller';
 import { AuthModule } from './infra/auth/auth.module';
 
 @Module({
   imports: [AuthModule],
-  controllers: [AppController, AuthController],
+  controllers: [AppController],
   providers: [AppService],
 })
 export class AppModule {}
--- src/infra/auth/auth.module.ts
+++ src/infra/auth/auth.module.ts
@@ -1,6 +1,5 @@
 import { Module } from '@nestjs/common';
 import { PassportModule } from '@nestjs/passport';
-import { AuthController } from './auth.controller';
 import { KeycloakStrategy } from './keycloak.strategy';
 
 @Module({
