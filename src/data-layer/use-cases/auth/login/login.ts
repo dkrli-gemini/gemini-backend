492c10e3096c6fe2431badc1c0c3516f8aacf619
Pedro Martineli <martinelipedro@gmail.com>
2025-02-26 16:08:18 -0300

    chore: add axios

--- src/data-layer/use-cases/auth/login/login.ts
+++ src/data-layer/use-cases/auth/login/login.ts
@@ -7,7 +7,13 @@ import {
 
 @Injectable()
 export class Login implements ILogin {
+  private readonly keycloakUrl = process.env.KEYCLOAK_URL;
+  private readonly realm = process.env.KEYCLOAK_REALM;
+  private readonly clientId = process.env.KEYCLOAK_CLIENT_ID;
+
   async execute(input: ILoginInput): Promise<ILoginOutput> {
-    
+    try {
+      const response = await axios
+    }
   }
 }
