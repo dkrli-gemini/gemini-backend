a3a2f97c9732384eb5ba394550961a4caaefe045
Pedro Martineli <martinelipedro@gmail.com>
2025-03-03 19:11:29 -0300

    feat: add swagger docs

--- src/main.ts
+++ src/main.ts
@@ -3,11 +3,21 @@ import { AppModule } from './app.module';
 import * as dotenv from 'dotenv';
 import * as session from 'express-session';
 import { KeycloakConfigService } from './infra/auth/keycloak.config';
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 
 dotenv.config();
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+
+  const config = new DocumentBuilder()
+    .setTitle('Gemini API')
+    .setVersion('0.1')
+    .addBearerAuth()
+    .build();
+
+  const document = SwaggerModule.createDocument(app, config);
+  SwaggerModule.setup('docs', app, document);
 
   app.use(
     session({
