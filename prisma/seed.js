38e54c377bd400aaf88abe81284333e4156e7798
Pedro Martineli <martinelipedro@gmail.com>
2025-03-03 12:36:08 -0300

    refactor: populate seed.js

--- prisma/seed.js
+++ prisma/seed.js
@@ -2,7 +2,7 @@ import { PrismaClient } from '@prisma/client';
 
 const prisma = new PrismaClient();
 
-const main = async () => {
+const addUser = async () => {
   const newUser = await prisma.user.create({
     data: {
       name: 'Test name',
@@ -13,8 +13,29 @@ const main = async () => {
   console.log('User created!', newUser);
 };
 
-main()
-  .catch((e) => console.log(error))
+const updateUser = async () => {
+  const updateUser = await prisma.user.updateMany({
+    where: { name: 'Test name' },
+    data: {
+      name: 'new Name',
+    },
+  });
+
+  console.log(updateUser);
+};
+
+const readUser = async () => {
+  const user = await prisma.user.findFirst({
+    where: {
+      name: 'Test name',
+    },
+  });
+
+  console.log(user);
+};
+
+updateUser()
+  .catch((e) => console.log(e))
   .finally(async () => {
     await prisma.$disconnect();
   });
