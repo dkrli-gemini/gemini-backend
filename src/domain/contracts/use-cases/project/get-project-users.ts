2f2ebd4a922c128012f71be1acce99bf1559c35d
Pedro Martineli <martinelipedro@gmail.com>
2025-03-12 14:31:47 -0300

    feat: add GetProjectUsers domain

--- src/domain/contracts/use-cases/project/get-project-users.ts
+++ src/domain/contracts/use-cases/project/get-project-users.ts
@@ -1,6 +1,15 @@
+import { IUseCase } from '../../use-case';
+
 export interface IGetProjectUsersInput {
   projectId: string;
 }
 
 export interface IGetProjectUsersOutput {}
 
+export abstract class IGetProjectUsers
+  implements IUseCase<IGetProjectUsersInput, IGetProjectUsersOutput>
+{
+  abstract execute(
+    input: IGetProjectUsersInput,
+  ): Promise<IGetProjectUsersOutput>;
+}
